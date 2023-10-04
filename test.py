5bc0719aa26914984a5e6de009d51a9aa361ce4e
Carlos Quiala <cquiala12@gmail.com>
2023-10-04 18:22:21 -0400

    Tests

--- test.py
+++ test.py
@@ -1,10 +1,49 @@
-import ast, json
+from fastapi.testclient import TestClient
+from main import app
 
-str = "{\"key1\": \"value1\"},"
+client = TestClient(app)
 
-obj = ast.literal_eval(str)
+def test_calculate_revenue_valid():
+    # Prueba un escenario válido
+    order_request = {
+        "orders": [
+            {"id": 1, "item": "Laptop", "quantity": 1, "price": 999.99, "status": "completed"},
+            {"id": 2, "item": "Smartphone", "quantity": 2, "price": 499.95, "status": "pending"}
+        ],
+        "criterion": "completed"
+    }
 
-json_str = json.dumps(obj)
+    response = client.post("/solution", json=order_request)
+    assert response.status_code == 201
+    assert response.json() == "999.99"
 
-print(json_str)
+def test_calculate_revenue_negative_price():
+    # Prueba un escenario con precio negativo
+    order_request = {
+        "orders": [
+            {"id": 1, "item": "Laptop", "quantity": 1, "price": -999.99, "status": "completed"},
+            {"id": 2, "item": "Smartphone", "quantity": 2, "price": 499.95, "status": "pending"}
+        ],
+        "criterion": "completed"
+    }
+
+    response = client.post("/solution", json=order_request)
+    assert response.status_code == 422
+    assert response.json() == {"detail": [
+    {
+      "loc": [
+        "body",
+        "orders",
+        0,
+        "price"
+      ],
+      "msg": "ensure this value is greater than or equal to 0.01",
+      "type": "value_error.number.not_ge",
+      "ctx": {
+        "limit_value": 0.01
+      }
+    }
+  ]}
+
+
 
